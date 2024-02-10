8de7577ca713100a299a4dfcdb2dee4361dc1799
ElvisMw <elvismwanthi@gmail.com>
2024-02-10 20:18:26 +0300

    error fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
-
-# Ensure the script exits on errors
-set -e
-
-# Update and install Nginx
+# Setup a web servers for the deployment of web_static.
 apt update -y
 apt install -y nginx
-
-# Create directories with correct permissions
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-
-# Create index.html file with correct permissions
 echo "<!DOCTYPE html>
 <html>
   <head>
@@ -21,18 +12,7 @@ echo "<!DOCTYPE html>
     <p>Nginx server test</p>
   </body>
 </html>" | tee /data/web_static/releases/test/index.html
-
-# Create symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Set ownership recursively
 chown -R ubuntu:ubuntu /data
-
-# Update Nginx configuration
-echo -e "\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" | sudo tee -a /etc/nginx/sites-enabled/default
-
-# Restart Nginx
-service nginx restart
-
-# Exit successfully
-exit 0
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
