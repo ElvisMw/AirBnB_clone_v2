17b41ab9048ffbb1b7f4d3c1e562ec66354191bf
ElvisMw <elvismwanthi@gmail.com>
2024-02-10 21:23:10 +0300

    deploy

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ env.user = "ubuntu"
 
 
 def do_clean(number=0):
-    """ CLEANS """
+    """ clean """
 
     number = int(number)
 
@@ -20,3 +20,4 @@ def do_clean(number=0):
     local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
     path = '/data/web_static/releases'
     run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    
