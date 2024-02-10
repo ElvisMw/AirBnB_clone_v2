15c18f4edf0e9b944e0f59fe4083a738d264e246
ElvisMw <elvismwanthi@gmail.com>
2024-02-10 12:43:51 +0300

    0 setup correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
-
-# Ensure the script exits on errors
-set -e
-
-# Update and install Nginx
-sudo apt update -y
-sudo apt install -y nginx
-
-# Create directories with correct permissions
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-# Create index.html file with correct permissions
+# Bash script tht sets up your web servers for the deployment of web_static
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 echo "<!DOCTYPE html>
 <html>
   <head>
@@ -20,19 +11,8 @@ echo "<!DOCTYPE html>
   <body>
     <p>Nginx server test</p>
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Set ownership recursively
-sudo chown -R ubuntu:ubuntu /data
-
-# Update Nginx configuration
-echo -e "\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" | sudo tee -a /etc/nginx/sites-enabled/default
-
-# Restart Nginx
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
-
-# Exit successfully
-exit 0
