395817080dcb4a961172bbcbea1138ad5372c7a3
ElvisMw <elvismwanthi@gmail.com>
2024-02-18 20:51:55 +0300

    web flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask web application with three routes:
+- /: displays "Hello HBNB!"
+- /hbnb: displays "HBNB"
+- /c/<text>: displays "C " followed by the value of the text
+variable (replace underscore _ symbols with a space)
+"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!' when accessing the root route."""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """Displays 'HBNB' when accessing the /hbnb route."""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """Displays 'C ' followed by the value of the text variable."""
+    return "C {}".format(escape(text.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
